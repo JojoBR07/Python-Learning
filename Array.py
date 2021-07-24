62df111bdd512019f92d58e94f3d3d18cd1b8327
Joilson Oliveira Telles <joilsonoliveiratelles@MacBook-Air-de-Joilson.local>
2021-07-24 15:36:24 -0300

    Segundo Commit

--- Array.py
+++ Array.py
@@ -12,3 +12,16 @@ no aprendizado.
 
     Segue alguns exemplos 🤓
 """
+
+numeros = [1, 2, 3] #exemplo de array
+print(type(numeros))
+
+numeros.append(3) #adicionando valores 
+numeros.append(4)
+
+print(len(numeros)) #exibindo tamanho do array
+
+numeros[3] = 20 #atribuindo valor a uma posição específica
+numeros.insert(0, -200) #inseri um registros na posição 0
+
+print(numeros[-1]) #imprimi sempre o ultimo valor
