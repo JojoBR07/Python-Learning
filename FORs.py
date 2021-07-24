312881c06aac31cd0fc223bfc7d97964f94904bd
Joilson Oliveira Telles <joilsonoliveiratelles@MacBook-Air-de-Joilson.local>
2021-07-24 19:07:24 -0300

    Quinto Commit

--- FORs.py
+++ FORs.py
@@ -20,3 +20,14 @@ for i in range(1, 100, 7):  # Vai de 1 ao 100 incrementando de 7 em 7
 lista = [1, 4, 8, 16, 20]
 for i in range(20, 0, -2):  # Vai de 20 ao 0 decrementando de 2 em 2
     print(i, end=' ')
+
+for i in range(0, 10):  
+    pass #comando permitir definir estrutura vazia
+
+for i in range(0, 10):  
+    while i == 1:
+        continue #Interrompe o while mas continua o for
+
+for i in range(0, 10):  
+    while i == 1:
+        break #Interrompe o while e o for
---
+++ WHILEs.py
@@ -0,0 +1,16 @@
+#!python3
+
+""" 
+    Olá Jovem Padawan 💻
+
+Estrutura de repetição WHILE
+
+    Segue alguns exemplos 🤓
+"""
+
+x = 0
+
+while x != -1:
+    x = float(input('Informe -1 Para Sair: '))
+
+print('Fim!')
